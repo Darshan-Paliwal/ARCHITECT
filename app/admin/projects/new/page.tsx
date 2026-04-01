fd870816c900415cd05c30c2847a13d59f16ee65
Darshan-Paliwal <darshanpaliwal.work@gmail.com>
2026-04-01 16:50:57 +0530

    Add NewProjectPage component with ProjectForm

---
+++ app/admin/projects/new/page.tsx
@@ -0,0 +1,10 @@
+import ProjectForm from '@/components/admin/ProjectForm';
+
+export default function NewProjectPage() {
+  return (
+    <div>
+      <h1 className="font-display text-3xl font-light mb-8">New Project</h1>
+      <ProjectForm />
+    </div>
+  );
+}
